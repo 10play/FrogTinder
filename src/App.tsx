1f5f52ef9affd09041c162ae20a09045ad0107b2
Amir Angel <36531255+17Amir17@users.noreply.github.com>
2024-11-15 15:52:12 +0700

    anayltics

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import { TinderSwiper } from "./TinderSwiper";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <div className="App">
       <TinderSwiper />
+      <Analytics />
     </div>
   );
 }
