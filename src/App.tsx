685b4fc6b6e912f6478ac73ee98b75fa8706d8c3
Amir Angel <36531255+17Amir17@users.noreply.github.com>
2024-11-15 16:32:11 +0700

    header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import { Header } from "./Header";
 import { TinderSwiper } from "./TinderSwiper";
 import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <TinderSwiper />
       <Analytics />
     </div>
